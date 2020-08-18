9274a479cf5150ac534993c59acded317b0cddec
kimgayoung <namiein@naver.com>
2020-08-18 15:35:48 +0900

    Updates

---
+++ precache-manifest.f4f96c2542806b0c7b4c443f0d89f8e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7efeae703abff83813da0137a681b755",
+    "url": "/movie_app_2020_v2/index.html"
+  },
+  {
+    "revision": "c1fa5a2cbd2869017d2e",
+    "url": "/movie_app_2020_v2/static/css/main.6a7e9332.chunk.css"
+  },
+  {
+    "revision": "81fcdb93250fda4efe34",
+    "url": "/movie_app_2020_v2/static/js/2.c1e298d2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020_v2/static/js/2.c1e298d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1fa5a2cbd2869017d2e",
+    "url": "/movie_app_2020_v2/static/js/main.2ea0e5da.chunk.js"
+  },
+  {
+    "revision": "f18144ab79c483c04d0d",
+    "url": "/movie_app_2020_v2/static/js/runtime-main.51d0d79a.js"
+  }
+]);
